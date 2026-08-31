a4ce0477e5aeda85d58f9628bc8fb213ffef0f28
Gael Isaac Vásquez Burgos <gaelvasbur@gmail.com>
2026-08-31 11:15:13 -0600

    Update 02_ClickCounter.jsx

--- src/components/02_ClickCounter.jsx
+++ src/components/02_ClickCounter.jsx
@@ -2,27 +2,21 @@ import React, { useState } from 'react';
 import './02_ClickCounter.css';
 
 function ClickCounter() {
-  // 1. Reemplazamos 'let numberOfClicks = 0' por un estado de React
   const [numberOfClicks, setNumberOfClicks] = useState(0);
 
-  // 2. La función que maneja el evento clic y actualiza el contador
   const handleCount = () => {
     setNumberOfClicks(numberOfClicks + 1);
   };
 
   return (
     <div className="counter-container">
-      
-      {/* Pintamos el estado directamente en el HTML de forma reactiva */}
       <div id="counter">Clics: {numberOfClicks}</div>
-      
-      {/* Añadimos el onClick que llama a nuestra función */}
       <button id="btn-contar" onClick={handleCount}>
         Count clics
       </button>
-
     </div>
   );
 }
 
 export default ClickCounter;
+
